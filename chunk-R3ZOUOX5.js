90129f96c2e6a6441eeab535375072913ed2aba6
clementcolter <clementcolter@gmail.com>
2026-02-23 22:48:41 -0600

    Auto-generated commit

---
+++ chunk-R3ZOUOX5.js
@@ -0,0 +1,28 @@
+import{Ca as f,Fa as s,Ga as l,Ha as m,Ia as i,La as h,Ma as u,Na as b,Oa as y,Pa as p,U as r,pa as d,pb as v,qa as g,ta as o}from"./chunk-ASASPVJH.js";function I(t,a){if(t&1&&i(0,"img",7),t&2){let e=h();s("src",e.image,g)}}var c=class t{title="";image="";author="Anonymous";date="";content="";static \u0275fac=function(e){return new(e||t)};static \u0275cmp=r({type:t,selectors:[["app-blog-post"]],inputs:{title:"title",image:"image",author:"author",date:"date",content:"content"},standalone:!0,features:[p],decls:9,vars:5,consts:[[1,"max-w-5xl","mx-auto","my-8"],[1,"bg-white","shadow-lg","rounded-lg","overflow-hidden"],["alt","Blog Image","class","w-full h-80 object-cover",3,"src",4,"ngIf"],[1,"p-6"],[1,"text-3xl","font-bold","text-gray-800","mb-2"],[1,"text-sm","text-gray-500","mb-4"],[1,"text-gray-700","leading-relaxed",3,"innerHTML"],["alt","Blog Image",1,"w-full","h-80","object-cover",3,"src"]],template:function(e,n){e&1&&(l(0,"div",0)(1,"div",1),f(2,I,1,1,"img",2),l(3,"div",3)(4,"h2",4),u(5),m(),l(6,"p",5),u(7),m(),i(8,"div",6),m()()()),e&2&&(o(2),s("ngIf",n.image),o(3),b(" ",n.title," "),o(2),y(" By ",n.author," \u2022 ",n.date," "),o(),s("innerHTML",n.content,d))},dependencies:[v],encapsulation:2})};var C=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=r({type:t,selectors:[["app-contribute"]],standalone:!0,features:[p],decls:1,vars:0,consts:[["title","Why Contributions Matter","image","contribute.jpg","author","Alpha Delta Phi Alumni Association","date","March 2025","content",`
+    <p>
+      <strong></strong> 
+      Our chapter isn't just about fostering growth for those pursuing their academic goals; 
+      it's a continuous evolution throughout life to build and strengthen connections in the chapter and Greek life as a whole. 
+      Part of what helps accelerate this process is contributions.
+    </p>
+
+    <p>
+      Our chapter grows stronger when members contribute ideas, time, and effort.
+      Contributions don\u2019t need to be huge. A small improvement, thoughtful feedback, a small donation,
+      or sharing knowledge can create ripple effects that benefit the entire community.
+    </p>
+
+    <p>
+      Growth happens when we build together. If you\u2019re thinking about contributing,
+      you\u2019re already part of the journey.
+    </p>
+    <div class='mt-6'>
+    <a 
+      href='https://us.commitchange.com/md/annapolis/1832foundation'
+      target='_blank'
+      class='inline-block px-6 py-3 bg-green-600 text-white font-semibold rounded-md shadow-md hover:bg-green-700 transition-all duration-200'
+    >
+      Contribute Today!
+    </a>
+  </div>
+  `]],template:function(e,n){e&1&&i(0,"app-blog-post",0)},dependencies:[c]})};export{C as ContributeComponent};
